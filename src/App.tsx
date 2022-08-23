ed875bc6e1e89b4ddb6097e2e537c9b2214ac73e
reachscript-jak <123ja.k123@gmail.com>
2022-08-23 14:35:50 +0900

    イベントハンドラ以外ではバッチ化されていないことの確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { AutoBatchEventHandler } from './components/AutoBatchEventHandler';
+import { AutoBatchOther } from './components/AutoBatchOther';
 
 function App() {
   return (
     <div className="App">
       <AutoBatchEventHandler />
+      <AutoBatchOther />
     </div>
   );
 }
